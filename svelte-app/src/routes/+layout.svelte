0f19e736484921e930bb20816a271821ee32b6dd
Saad5400 <sdbtwa@gmail.com>
2023-10-11 23:36:38 +0300

    starting new

--- svelte-app/src/routes/+layout.svelte
+++ svelte-app/src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+	import { autoModeWatcher } from '@skeletonlabs/skeleton';
 	import '../app.postcss';
 </script>
+
+<svelte:head>{@html `<script>${autoModeWatcher.toString()} autoModeWatcher();</script>`}</svelte:head>
 
 <slot />
