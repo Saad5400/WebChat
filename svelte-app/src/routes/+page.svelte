5d4360b4b30574ade762bebc246f5d602d044203
Saad5400 <sdbtwa@gmail.com>
2023-10-12 17:47:21 +0300

    some changes

--- svelte-app/src/routes/+page.svelte
+++ svelte-app/src/routes/+page.svelte
@@ -14,14 +14,50 @@
         minNumbers: 0,
         minSymbols: 0,
     });
+
+    // scrolling
+    let y = 0;
+    let lastY = 0;
+
+    let register: any;
+    let landing: any;
+
+    let scrolling = false;
+
+    const scrollToRegister = (y) => {
+        let dy = lastY - y;
+        lastY = y;
+
+        if (!scrolling && (dy < -10 || dy > 10)) {
+            scrolling = true;
+            setTimeout(() => {
+                scrolling = false;
+            }, 500);
+            if (dy < -10) scrollTo(0, register.getBoundingClientRect().top + y);
+            if (dy > 10) scrollTo(0, landing.getBoundingClientRect().top + y);
+        }
+    };
+
+    $: scrollToRegister(y);
+
+    // register
+    const registerSubmit = (e) => {
+        e.preventDefault();
+        if (validEmail && validPassword) {
+            console.log("registering");
+        }
+    };
 </script>
+
+<svelte:window bind:scrollY={y} />
 
 <div
     class="flex items-center w-full min-h-[100dvh] max-w-xs sm:max-w-md md:max-w-xl lg:max-w-2xl xl:max-w-3xl mx-auto"
+    bind:this={landing}
 >
-    <h2 class="text-center h2 gradient-heading1">
+    <h2 class="text-center h2 gradient-text gradient-heading1">
         Welcome to the
-        <h1 class="h1 gradient-heading2">New WhatsApp</h1>
+        <h1 class="h1 gradient-text gradient-heading2">New WhatsApp</h1>
         <h3 class="h3">
             We provide complete privacy with a seamless experience
         </h3>
@@ -31,12 +67,19 @@
     <div
         class="w-full bg-surface-900 shadow-2xl p-6 mx-auto max-w-md sm:max-w-lg rounded-lg"
     >
-        <h4 class="h4 mb-10 !font-normal gradient-heading1">
-            So, what are you waiting for? <h2 class="h2 text-center">
+        <h4 class="h4 mb-4 !font-normal gradient-text gradient-heading1">
+            So, what are you waiting for? <h2
+                class="h2 text-center leading-loose"
+            >
                 Register Now!
             </h2>
         </h4>
-        <form class="flex flex-col gap-4">
+        <form
+            method="post"
+            class="flex flex-col gap-4"
+            bind:this={register}
+            on:submit={registerSubmit}
+        >
             <label class="label">
                 <span>Email Address</span>
                 <input
@@ -47,6 +90,8 @@
                     class:input-error={email?.length >= minInputLength &&
                         !validEmail}
                     bind:value={email}
+                    required
+                    minlength={minInputLength}
                 />
             </label>
             <label class="label">
@@ -59,19 +104,22 @@
                     class:input-error={password?.length >= minInputLength &&
                         !validPassword}
                     bind:value={password}
+                    required
+                    minlength="8"
                 />
             </label>
+            <button
+                type="submit"
+                class="btn rounded-md bg-gradient-to-br variant-gradient-primary-tertiary text-primary-50"
+            >
+                Register/Login
+            </button>
         </form>
     </div>
 </div>
 
 <style lang="postcss">
-    .gradient-text {
-        @apply bg-clip-text text-transparent box-decoration-clone;
-    }
-
     .gradient-heading1 {
-        @apply gradient-text;
         /* Direction */
         @apply bg-gradient-to-br;
         /* Color Stops */
@@ -79,7 +127,6 @@
     }
 
     .gradient-heading2 {
-        @apply gradient-text;
         /* Direction */
         @apply bg-gradient-to-tl;
         /* Color Stops */
