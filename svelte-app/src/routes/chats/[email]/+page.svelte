f6292beecbef89e2d57ee5093ca15379e6c1f059
Saad5400 <sdbtwa@gmail.com>
2023-10-23 22:01:22 +0300

    some ui ux changes

--- svelte-app/src/routes/chats/[email]/+page.svelte
+++ svelte-app/src/routes/chats/[email]/+page.svelte
@@ -56,7 +56,7 @@
 	}
 
 	function onPromptKeydown(event: KeyboardEvent): void {
-		if (["Enter"].includes(event.code)) {
+		if (["Enter"].includes(event.code) && !event.shiftKey) {
 			event.preventDefault();
 			sendMessage();
 		}
@@ -176,7 +176,9 @@
 								{bubble.createdAt}
 							</small>
 						</header>
-						<p>{bubble.text}</p>
+						<p class="whitespace-pre">
+							{bubble.text}
+						</p>
 					</div>
 				</div>
 			{/each}
@@ -194,7 +196,6 @@
 				name="prompt"
 				id="prompt"
 				placeholder="Write a message..."
-				rows="1"
 				on:keydown={onPromptKeydown}
 			/>
 			<button
@@ -203,7 +204,20 @@
 					: "input-group-shim"}
 				on:click={sendMessage}
 			>
-				<i class="fa-solid fa-paper-plane" />
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="w-6 h-6"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
+					/>
+				</svg>
 			</button>
 		</div>
 	</section>
